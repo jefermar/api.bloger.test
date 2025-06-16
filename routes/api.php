e2969fe5708016b423421f58cc71a17caa4889e5
jefermar <jefermar17jjs@gmail.com>
2025-06-16 15:45:43 -0500

    hola

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComputerController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+ Route::get('computer', [ComputerController::class,'index'])->name('api.v1.computer.index');
